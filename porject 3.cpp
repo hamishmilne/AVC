b4351a8eb40e27127b7546af0c7411b4f1d1abf3
knottjames <50603991+knottjames@users.noreply.github.com>
2019-05-21 14:34:03 +1200

    Add files via upload

---
+++ porject 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "E101.h"
+int main(){
+	
+	
+char inital[] = "Please";
+char add[] = "130.195.6.196"
+int = 1024;
+int connect_to_server(add[],port);
+
+int send_to_server(inital[]);
+int receive_from_server(char message[24]);
+int send_to_server(char message[24]);
+
+}
