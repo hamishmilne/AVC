ba24a8c613f50c8eec7632902fbe8cf645ac2aa9
Adamkwiecien <kwieciadam@myvuw.ac.nz>
2019-05-21 02:53:29 +0000

    working

--- porject 3.cpp
+++ porject 3.cpp
@@ -3,10 +3,10 @@
 int main(){
 	
 	
-char inital[24] = {'P','l','e','a','s','e''};
+char inital[24] = {'P','l','e','a','s','e'};
 char add[15] = {'1','3','0','.','1','9','5','.','6','.','1','9','6'};
 int port = 1024;
-		   char pass[24];
+char pass[24];
 connect_to_server(add,port);
 
 send_to_server(inital);
