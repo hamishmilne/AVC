a8f39ab610d90328562cec4476688298c12aff9a
knottjames <50603991+knottjames@users.noreply.github.com>
2019-05-21 14:47:46 +1200

    Update porject 3.cpp

--- porject 3.cpp
+++ porject 3.cpp
@@ -3,7 +3,7 @@
 int main(){
 	
 	
-char inital[24] = {'P','l','e','a','s','e''};
+char inital[24] = {'P','l','e','a','s','e'};
 char add[15] = {'1','3','0','.','1','9','5','.','6','.','1','9','6'};
 int port = 1024;
 		   char pass[24];
