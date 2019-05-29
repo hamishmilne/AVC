b26f8f508d2342a02f97c5e4c22d9a1cfe5e6e6f
Adamkwiecien <kwieciadam@myvuw.ac.nz>
2019-05-29 02:20:55 +0000

    check

--- main.cpp
+++ main.cpp
@@ -110,6 +110,29 @@ void Robot::stop() {
     right.stop();
 }
 
+bool getBlack(int x, int y) {
+    int r = get_pixel(x, y, 0);
+    int g = get_pixel(x, y, 1);
+    int b = get_pixel(x, y, 2);
+
+    //Normalize and gamma correct
+    float rr = pow(r / 255.0, 2.2);
+    float gg = pow(g / 255.0, 2.2);
+    float bb = pow(b / 255.0, 2.2);
+
+    //Calculate luminance
+    float lum = 0.2126 * rr + 0.7152 * gg + 0.0722 * bb;
+
+    // Gamma compand and rescale to byte range:
+    int greyLevel = (int) (255.0 * pow(lum, 1.0 / 2.2));
+    
+    if(greyLevel > 255/2) {
+		return false;
+	}else{
+		return true;
+	}
+}
+
 int Robot::checkLine() {
 
     take_picture();
@@ -126,46 +149,39 @@ int Robot::checkLine() {
 
         //Left side of screen
         for(int x = 0; x < width / 3; x++) {
-            if(getBlack(x, y)) blackLeft++;
+            if(getBlack(x, y)) {
+				 blackLeft++;
+			}
         }
 
         //Middle of screen
         for(int x = width / 3; x < width / 3 * 2; x++) {
-            if(getBlack(x, y)) blackMiddle++;
+            if(getBlack(x, y)) {
+				blackMiddle++;
+			}
         }
 
         //Right side of screen
         for(int x = width / 3 * 2; x <  width; x++) {
-            if(getBlack(x, y)) blackRight++;
+            if(getBlack(x, y)) {
+				blackRight++;
+			}
         }
     }
+    
+    //std::cout << blackLeft << std::endl;
+    //std::cout << blackMiddle << std::endl;
+    //std::cout << blackRight << std::endl;
 
     if(blackLeft > blackMiddle && blackLeft > blackRight) return 0;
 
     if(blackMiddle > blackLeft && blackMiddle > blackRight) return 1;
 
     if(blackRight > blackLeft && blackRight > blackMiddle) return 2;
+    
+
+    return 3;
 }
-
-bool getBlack(int x, int y) {
-    int r = get_pixel(x, y, 0);
-    int g = get_pixel(x, y, 1);
-    int b = get_pixel(x, y, 2);
-
-    //Normalize and gamma correct
-    float rr = pow(r / 255.0, 2.2);
-    float gg = pow(g / 255.0, 2.2);
-    float bb = pow(b / 255.0, 2.2);
-
-    //Calculate luminance
-    float lum = 0.2126 * rr + 0.7152 * gg + 0.0722 * bb;
-
-    // Gamma compand and rescale to byte range:
-    int greyLevel = (int) (255.0 * pow(lum, 1.0 / 2.2));
-    int grey = (greyLevel << 16) + (greyLevel << 8) + greyLevel; 
-    return grey;
-}
-
 
 
 //Main code
@@ -179,9 +195,9 @@ int main() {
     open_screen_stream();
 
     while(true) {
-        std::cout << r.checkLine << std:: endl;
+        std::cout << r.checkLine() << std::endl;
         update_screen();
-        sleep1(500);
+        sleep1(300);
     }
 
 
