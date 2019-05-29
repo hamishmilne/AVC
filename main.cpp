387f8a81e3e5450b327d6fe5be239851a5be061e
Hamish Milne <milnehta@hotmail.com>
2019-05-29 13:51:03 +1200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include "E101.h"
+#include <math.h>
 
 class Motor {
-    unsigned char num;              //Motor num
-    unsigned char pwm;              //Motor speed
+    unsigned char num;              //Motor num 
+    unsigned char pwm;              //Motor speed 
     bool left;                      //Is the motor on the left or right?
-    
-public:
-    Motor();                    //Dont use
-    Motor(unsigned char motor_num, unsigned char motor_speed, bool motor_left);
-    void forward();
-    void back();
-    void stop();
+
+    public:
+        Motor();                    //Dont use
+        Motor(unsigned char motor_num, unsigned char motor_speed, bool motor_left);
+        void forward();
+        void back();
+        void stop();
 };
 
 class Robot {
     Motor left;                     //Left motor
     Motor right;                    //Right motor
     
-public:
-    Robot();                    //Dont use
-    Robot(Motor motor_left, Motor motor_right);
-    void startLeft();
-    void startRight();
-    void startForward();
-    void stop();
+    public:
+        Robot();                    //Dont use
+        Robot(Motor motor_left, Motor motor_right);
+        void startLeft();
+        void startRight();
+        void startForward();
+        void stop();
+        int checkLine();
 };
 
 //Motor constructer, usage: Motor m(num, pwn);
@@ -34,6 +36,11 @@ Motor::Motor(unsigned char motor_num, unsigned char motor_speed, bool motor_left
     left = motor_left;
     set_motors(num, pwm);
     hardware_exchange();
+}
+
+//Does nothing
+Motor::Motor() {
+
 }
 
 //Move motor forward
@@ -70,8 +77,13 @@ void Motor::stop() {
 Robot::Robot(Motor motor_left, Motor motor_right) {
     left = motor_left;
     right = motor_right;
-    
+
     //Create camera here etc
+}
+
+//Does nothing
+Robot::Robot() {
+
 }
 
 //Moves both motors forward
@@ -98,13 +110,80 @@ void Robot::stop() {
     right.stop();
 }
 
+int Robot::checkLine() {
+
+    take_picture();
+
+    int width = 240;
+    int height = 320;
+
+    int blackLeft = 0;
+    int blackMiddle = 0;
+    int blackRight = 0;
+
+    //Loop through each col
+    for(int y = 0; y < height; y++) {
+
+        //Left side of screen
+        for(int x = 0; x < width / 3; x++) {
+            if(getBlack(x, y)) blackLeft++;
+        }
+
+        //Middle of screen
+        for(int x = width / 3; x < width / 3 * 2; x++) {
+            if(getBlack(x, y)) blackMiddle++;
+        }
+
+        //Right side of screen
+        for(int x = width / 3 * 2; x <  width; x++) {
+            if(getBlack(x, y)) blackRight++;
+        }
+    }
+
+    if(blackLeft > blackMiddle && blackLeft > blackRight) return 0;
+
+    if(blackMiddle > blackLeft && blackMiddle > blackRight) return 1;
+
+    if(blackRight > blackLeft && blackRight > blackMiddle) return 2;
+}
+
+bool getBlack(int x, int y) {
+    int r = get_pixel(x, y, 0);
+    int g = get_pixel(x, y, 1);
+    int b = get_pixel(x, y, 2);
+
+    //Normalize and gamma correct
+    float rr = pow(r / 255.0, 2.2);
+    float gg = pow(g / 255.0, 2.2);
+    float bb = pow(b / 255.0, 2.2);
+
+    //Calculate luminance
+    float lum = 0.2126 * rr + 0.7152 * gg + 0.0722 * bb;
+
+    // Gamma compand and rescale to byte range:
+    int greyLevel = (int) (255.0 * pow(lum, 1.0 / 2.2));
+    int grey = (greyLevel << 16) + (greyLevel << 8) + greyLevel; 
+    return grey;
+}
+
+
+
 //Main code
 int main() {
-    
-    Motor left(3, 48, true);
-    Motor right(4, 48, false);
+    init(0);
+
+    Motor left(3, 48, true);    //Left motor is on port 3
+    Motor right(4, 48, false);  //Right motor is on port 4
     Robot r(left, right);
-    
-    
-    return 0;
+
+    open_screen_stream();
+
+    while(true) {
+        std::cout << r.checkLine << std:: endl;
+        update_screen();
+        sleep1(500);
+    }
+
+
+    return 0;   
 }
